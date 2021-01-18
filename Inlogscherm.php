4b2d884c0d41888c8713a62b3c5afaeb2367045f
Jared Sackey <jared.sackey@student.rocva.nl>
2021-01-18 15:30:31 +0100

    kleine vraag meester moren omhulp

--- Inlogscherm.php
+++ Inlogscherm.php
@@ -1,3 +1,30 @@
+<?php
+
+require 'database.php';
+
+if(isset($_POST['form_login'])){
+
+  $email    =$_POST['form_email'];
+  $password =$_POST['form_password'];
+
+  $sql = "SELECT * FROM users WHERE email= :ph_email";
+  $statement = $db_conn->prepare($sql);
+  $statement->bindParam(":ph_email", $email); 
+  $statement->execute();
+  $database_gegevens= $statement->fetch(PDO::FETCH_ASSOC);
+
+  if($database_gegevens != FALSE){
+
+        if($database_gegevens['password'] == $password){
+            echo'gebruiker mag inloggen!';
+        }
+  }
+ 
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,13 +37,16 @@
 <body class="text-center">
     
     <main class="form-signin">
-      <form>
+      <form method="post" action="">
         <h1 class="h3 mb-3 fw-normal"> Sign In</h1>
-        <label for="inputEmail" class="visually-hidden">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="visually-hidden">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+
+        <label for="form_email" class="visually-hidden">Email address</label>
+        <input type="email" id="form_email" class="form-control" name="form_email" placeholder="Email address" required autofocus>
+
+        <label for="form_password" class="visually-hidden">Password</label>
+        <input type="password" id="form_password" name="form_password" class="form-control" placeholder="Password" required>
+
+        <button class="w-100 btn btn-lg btn-warning" type="submit" name="form_login">Sign in</button>
         <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
       </form>
     </main>
