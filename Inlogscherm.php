fc4ac926f0a0deaf682a39d4f1d3292e6f818bd7
Jared Sackey <jared.sackey@student.rocva.nl>
2021-01-18 12:21:24 +0100

    Beginnen

--- Inlogscherm.php
+++ Inlogscherm.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="signin.css" rel="stylesheet">
+    <title>HandigeMannen</title>
 </head>
-<body>
+<body class="text-center">
     
-</body>
+    <main class="form-signin">
+      <form>
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        <label for="inputEmail" class="visually-hidden">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="visually-hidden">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
+      </form>
+    </main>
+    
+    
+        
+      </body>
 </html>
