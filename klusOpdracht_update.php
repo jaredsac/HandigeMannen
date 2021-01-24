10a0e631982c91d7cc0e0d5a8fde461219b94ee7
Jared Sackey <jared.sackey@student.rocva.nl>
2021-01-24 19:06:27 +0100

    update

--- klusOpdracht_update.php
+++ klusOpdracht_update.php
@@ -78,7 +78,7 @@ header('location: klusOpdracht_index.php');
   <body>
     
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Klusbedrijf Handige Mannen</a>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="klusOpdracht_index.php">Klusbedrijf Handige Mannen</a>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
