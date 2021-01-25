ba9af6d123f98394037d3eb2d32d1f39437bf0c9
Jared Sackey <jared.sackey@student.rocva.nl>
2021-01-25 13:18:51 +0100

    update

--- header.php
+++ header.php
@@ -6,3 +6,4 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Klus bedrijf Handige Mannen</title>
 </head>
+</html>
